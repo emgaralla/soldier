52e9ece8f94f26e6c54fa799574b65c5f73558fd
Roscoe <emgaralla@mail.ru>
2023-05-16 21:35:39 +0300

    Создал объект

---
+++ soldat.js
@@ -0,0 +1,13 @@
+const soldier = {
+    name: 'Халк',
+    hp: 10,
+    weapon: {
+      weaponName: 'ПП',
+      patrons: 30,
+    },
+    ammunition: 3,
+  
+    fire: function () {},
+    recharge: function () {},
+    hurt: function () {},
+  };
